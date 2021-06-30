2b24b5564f9f27be5e13f0fc8737b2660efad943
Towhid Chowdhury <mac9908@rit.edu>
2021-06-29 22:07:36 -0400

    boop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
 
+    $('#submit-button').on('click', function(e) {
+        e.preventDefault();
+
+        var data = $(this.form).serialize();
+        console.log(data);
+
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+
+        $.post('https://script.google.com/macros/s/AKfycbymCCzMa18igJ7XxuWsrirYyJ94tSxScfw-g5OKMRgTnyhtDzYqoNzGVyfFifOtsXs_/exec', data)
+            .done(function (data) {
+                console.log(data);
+                if (data.result === "error") {
+                    $('#alert-wrapper').html(alert_markup('danger', data.message));
+                } else {
+                    $('#alert-wrapper').html('');
+                    $('#rsvp-modal').modal('show');
+                }
+            })
+            .fail(function (data) {
+                console.log(data);
+                $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+            });
+    });
     /***************** Waypoints ******************/
 
     $('.wp1').waypoint(function () {
@@ -209,6 +232,7 @@ $(document).ready(function () {
 
     /********************** RSVP **********************/
     $('#rsvp-form').on('submit', function (e) {
+        debugger;
         e.preventDefault();
         var data = $(this).serialize();
 
@@ -228,25 +252,6 @@ $(document).ready(function () {
                 console.log(data);
                 $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
             });
-        // $.ajax({
-        //     url: 'https://script.google.com/macros/s/AKfycbymCCzMa18igJ7XxuWsrirYyJ94tSxScfw-g5OKMRgTnyhtDzYqoNzGVyfFifOtsXs_/exec',
-        //     method: "POST",
-        //     dataType: "json",
-        //     data: data,
-        //     success: function(response) {
-        //         console.log(response);
-        //         if (response.result === "error") {
-        //             $('#alert-wrapper').html(alert_markup('danger', response.message));
-        //         } else {
-        //             $('#alert-wrapper').html('');
-        //             $('#rsvp-modal').modal('show');
-        //         }
-        //     },
-        //     error: function(response) {
-        //         console.log(response);
-        //         $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-        //     }
-        // })
     });
 
 });
